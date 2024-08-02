ab3765390f8620d302e505a8bddc3063a845fa5f
Aamish Hussain <aamishhussain23@gmail.com>
2024-08-02 17:08:54 +0530

    added local url

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ dotenv.config({
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin : [process.env.FRONTEND_URL],
+    origin : [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2],
     methods : ["GET", "POST", "PUT", "DELETE"],
     credentials : true
 }))
